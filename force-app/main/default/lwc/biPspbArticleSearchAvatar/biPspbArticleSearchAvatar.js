f9a9179b5af9f57e02ec6bc5d54068779c27ea29
nagsvalheal <158153501+nagsvalheal@users.noreply.github.com>
2024-08-21 10:43:03 +0530

    21081040 Commit

---
+++ force-app/main/default/lwc/biPspbArticleSearchAvatar/biPspbArticleSearchAvatar.js
@@ -0,0 +1,56 @@
+// This lightning web component is used to display the avatar message in the Information Center search article Page
+// To import Libraries
+import { LightningElement, wire, api } from 'lwc';
+// To import Apex Classes
+import GET_LOGGEDIN_USER_ACCOUNT from '@salesforce/apex/BI_PSP_CurrentUser.getEnrolleeRecords';
+// To import Static Resource
+import DEFAULT_IMG from '@salesforce/resourceUrl/BI_PSPB_ProfileAvatar';
+// To import Custom Labels
+import { LABELS } from 'c/biPspbLabelForInfoCenter';
+
+export default class BiPspbArticleSearchAvatar extends LightningElement {
+	@api siteUrlq;
+	renderedCount = 0;
+	cardImage = '';
+	articleSearchAvatarMessage = LABELS.ARTICLE_SEARCH_AVATAR_MESSAGE;
+	acuteMessage = this.articleSearchAvatarMessage.substring(0,128) + '...';
+	// Method to display message for mobile
+	displayMessage() {
+		this.acuteMessage = this.articleSearchAvatarMessage.substring(0,128) + '...';
+		this.template.querySelector('.paranew').style.display = 'block';
+	}
+
+	// Method to display message for desktop
+	displayExpandedMessage() {
+		this.acuteMessage = this.articleSearchAvatarMessage;
+		this.template.querySelector('.paranew').style.display = 'none';
+	}
+
+	
+	// To retrieve the logged in user name and selected avatar
+	@wire(GET_LOGGEDIN_USER_ACCOUNT)
+	wiredUserDetails({ error, data }) {
+		try {
+		if (data) {
+			this.cardImage = data[0]?.BI_PSP_AvatarUrl__c
+			? data[0]?.BI_PSP_AvatarUrl__c
+			: DEFAULT_IMG;
+			const event = new CustomEvent('childrendered', {
+				detail: { rendered: true }
+			});
+			this.dispatchEvent(event);
+		} else if (error) {
+			this.navigateToErrorPage(error.body.message); // Catching Potential Error from Apex
+		}
+		} catch (err) {
+		this.navigateToErrorPage(err.message); // Catching Potential Error from Lwc
+		}
+	}	
+
+	// navigateToErrorPage used for all the error messages caught
+	navigateToErrorPage(errorMessage) {
+		let globalThis = window;
+		globalThis.sessionStorage.setItem('errorMessage', errorMessage);
+		globalThis.location.assign(this.siteUrlq + LABELS.ERROR_PAGE); 
+	}
+}
